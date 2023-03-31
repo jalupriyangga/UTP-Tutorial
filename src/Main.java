badae42e6635fa9ed57e96a261029e9f6040aebe
jalupriyangga <dionisiusaritojp@gmail.com>
2023-03-31 20:22:23 +0700

    berhasil run

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     // Coba Cek soalnya ga muncul run nya
-    System.out.println("Hello World!");
+    System.out.println("Akhirnnya berhasil, jadi caranya itu");
+    System.out.println("1. src nya dibikin Mark Directory as Source Root terus outputnya di pilih ke alamat folder yang sama");
   }
 }
